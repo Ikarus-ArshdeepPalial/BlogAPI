b8e2503d1b7f2d07803d1ca96f611d62504b276f
Ikarus-ArshdeepPalial <arshdeep.palial@ikarus3d.com>
2026-01-07 19:33:25 +0530

    hello

--- Backend/blog/signals.py
+++ Backend/blog/signals.py
@@ -1,8 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Blog
-from .blog_ai_utils.Generate_summary import generate_summary
-from .blog_ai_utils.Blog_category import get_category
 
 @receiver(post_save, sender=Blog)
 def populate_summary_and_category(sender, instance, created, **kwargs):
@@ -10,6 +8,8 @@ def populate_summary_and_category(sender, instance, created, **kwargs):
     Automatically populates the summary and category for a new blog post.
     """
     if created:
+        from .blog_ai_utils.Generate_summary import generate_summary
+        from .blog_ai_utils.Blog_category import get_category
         summary = generate_summary(instance.content)
         category = get_category(instance.content)
         Blog.objects.filter(pk=instance.pk).update(summary=summary, category=category)
