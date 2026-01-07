e403f78b85c9b0a51b541b883e4143dd3f8bbf5f
Ikarus-ArshdeepPalial <arshdeep.palial@ikarus3d.com>
2026-01-07 08:36:42 +0530

    Update settings.py

--- Backend/Project/settings.py
+++ Backend/Project/settings.py
@@ -77,10 +77,23 @@ WSGI_APPLICATION = 'Project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# settings.py
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'devdb'),
+        'USER': os.getenv('DB_USER', 'devuser'),
+        'PASSWORD': os.getenv('DB_PASS', 'changeme'),
+        'HOST': os.getenv('DB_HOST', 'db'), # 'db' should match the service name in docker-compose
+        'PORT': '5432',
     }
 }
 
